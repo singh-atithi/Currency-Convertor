a9ed7d54bc872f0eebb3d0aa2742bbbea485fe28
singh-atithi <singhatithi02@gmail.com>
2024-07-24 20:15:43 +0530

    DONEEEEEEE

--- app.js
+++ app.js
@@ -1,37 +1,49 @@
-let url="https://api.metalpriceapi.com/v1/latest?api_key=bbfdfd44e5c10f3192ee8554883c5c7c&base=USD&currencies=JPY";
-const msg=document.querySelector(".msg-container");
-let input=document.querySelector(".input-val input");
-// let inputval=input.value;
-// let fromsel=document.querySelector("#selto select");
-// let tosel=document.querySelector(".to select");
-// console.log(fromsel);
+let url =
+  "https://api.metalpriceapi.com/v1/latest?api_key=bbfdfd44e5c10f3192ee8554883c5c7c&base=";
+const msg = document.querySelector(".msg-container");
+let input = document.querySelector(".input-val input");
+const selects = document.querySelectorAll(".selector-boxes select");
 
-
-const selects=document.querySelectorAll(".selector-boxes select");
-
-for(let country of selects){
-    for(let currcode in countryList){
-        let newoption=document.createElement("option");
-        newoption.innerText=currcode;
-        newoption.value=currcode;
-        if(country.name==="from" && currcode==="USD"){
-            newoption.selected="selected";
-        }else if(country.name==="to" && currcode==="INR"){
-            newoption.selected="selected"
-        }
-        country.append(newoption);
+// ACCESS COUNTRY AND ITS CODE
+for (let country of selects) {
+  for (let currcode in countryList) {
+    let newoption = document.createElement("option");
+    newoption.innerText = currcode;
+    newoption.value = currcode;
+    if (country.name === "from" && currcode === "USD") {
+      newoption.selected = "selected";
+    } else if (country.name === "to" && currcode === "INR") {
+      newoption.selected = "selected";
     }
-    country.addEventListener("change",(evt)=>{
-        updateFlag(evt.target);
-    })
+    country.append(newoption);
+  }
+  country.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  });
 }
 
-const updateFlag=(element)=>{
-    let currCode=element.value;
-    let countrycode=countryList[currCode];
-    let image=element.parentElement.querySelector('img');
-    image.src=`https://flagsapi.com/${countrycode}/flat/64.png`
-}
+// UPDATE FLAG WITH SELECTION
+const updateFlag = (element) => {
+  let currCode = element.value;
+  let countrycode = countryList[currCode];
+  let image = element.parentElement.querySelector("img");
+  image.src = `https://flagsapi.com/${countrycode}/flat/64.png`;
+};
 
+// BUTTON WORKING FUNCTION
+let btn = document.querySelector("button");
+let btnclick = btn.addEventListener("click", async () => {
+  let inputval = input.value;
+  let fromsel = document.querySelector("#selto1");
+  let fromval = fromsel.value;
+  let tosel = document.querySelector("#selto2");
+  let toval = tosel.value;
+  const url2 = `${url}${fromval}&currencies=${toval}`;
 
-msg.innerText=`${inputval} `;
+  let data = await fetch(url2);
+  console.log(data);
+  let jsdata = await data.json();
+  console.log(jsdata.rates);
+  let rate = jsdata.rates[toval] * inputval;
+  msg.innerText = `${inputval} ${fromval} = ${rate} ${toval}`;
+});
